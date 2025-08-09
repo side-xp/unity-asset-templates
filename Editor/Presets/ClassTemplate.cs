055683b46e4e274cbf28cbe878b73cd2e8b4d7a5
Hubert Grossin <hubert.grossin@gmail.com>
2025-08-09 17:30:57 +0200

    feat: prefix/suffix pattern utility

--- Editor/Presets/ClassTemplate.cs
+++ Editor/Presets/ClassTemplate.cs
@@ -20,25 +20,9 @@ namespace SideXP.AssetTemplates.EditorOnly
     {
 
         /// <summary>
-        /// Matches the "class" or "struct" prefix, whether it uses a first lower or uppercase letter, but make sure the prefix is followed
-        /// by a space or an uppercase letter.<br/>
-        /// It outputs two groups:<br/>
-        /// - "prefix": the used prefix (you should use <see cref="string.ToLower()"/> to normalize this value)<br/>
-        /// - "name": the remaining part without the prefix (you should use <see cref="string.Trim()"/> on this value, as it may contaiin
-        /// the preceeding space used for the prefix)
+        /// The pattern for matching prefix or suffix.
         /// </summary>
-        private static readonly Regex PrefixPattern = new Regex(@"^(?<prefix>(?:[Cc]lass)|(?:[Ss]truct))(?=\s|[A-Z])(?<name>\s*\w+)");
-
-        /// <summary>
-        /// Matches the following suffixes:<br/>
-        /// - space + "class" lowercase<br/>
-        /// - space + "struct" lowercase<br/>
-        /// - "Class" with first letter uppercase<br/>
-        /// - "Struct" with first letter uppercase<br/>
-        /// It outputs the group "suffix". You should use <see cref="string.ToLower()"/> to normalize this valu, and
-        /// <see cref="string.Trim()"/> since this group also includes the starting space if any.
-        /// </summary>
-        private static readonly Regex SuffixPattern = new Regex(@"(?<suffix>(?:(?:\s+class)|(?:\s+struct))|(?:\s*Class)|(?:\s*Struct))$");
+        private static PrefixSuffixPattern s_pattern = new PrefixSuffixPattern("class", "struct");
 
         [Tooltip("If checked, the [Serializable] attribute will be added to the generated class.")]
         public bool SerializableByDefault = true;
@@ -46,26 +30,22 @@ namespace SideXP.AssetTemplates.EditorOnly
         /// <inheritdoc cref="IAssetTemplate.CanGenerateAsset(AssetInfo)"/>
         public bool CanGenerateAsset(AssetInfo info)
         {
-            // Try match prefix
-            Match match = PrefixPattern.Match(info.Name);
-            if (match.Success)
-                return true;
-
-            // Try match suffix
-            return SuffixPattern.Match(info.Name).Success;
+            return s_pattern.Match(info.Name, out _, out _);
         }
 
         /// <inheritdoc cref="IAssetTemplate.GenerateAsset(AssetInfo, ref AssetOutputInfo)"/>
         public bool GenerateAsset(AssetInfo info, ref AssetOutputInfo output)
         {
             // Cancel if the class name can't be processed
-            if (!ParseClassName(info.Name, out bool isClass, out string className))
+            if (!s_pattern.Match(info.Name, out string className, out string matchingPart))
             {
-                Debug.LogError("Faield to generate class (or struct) from the \"Class\" asset template: invalid class name");
+                Debug.LogError("Faield to generate class (or struct) from the \"Class / Struct\" asset template: invalid class name");
                 return false;
             }
 
+            bool isClass = matchingPart.ToLower() == "class";
             ScriptGenerator scriptGenerator = new ScriptGenerator(info);
+
             // If the user wants to generate a class script, apply inheritance
             if (isClass)
             {
@@ -90,34 +70,6 @@ namespace SideXP.AssetTemplates.EditorOnly
             output.Content = scriptGenerator.Generate();
 
             return true;
-        }
-
-        /// <summary>
-        /// Parse the given file name to get information about the expected output.
-        /// </summary>
-        /// <param name="name">The name of the file, as typed by the user.</param>
-        /// <param name="isClass">Should the output script delcare a class or a struct?</param>
-        /// <param name="className">Ouptuts the expected class name, without prefix or suffix.</param>
-        /// <returns>Returns true if the given file name has been parsed successfully.</returns>
-        private bool ParseClassName(string name, out bool isClass, out string className)
-        {
-            // Try match prefix
-            Match match = PrefixPattern.Match(name);
-            if (match.Success)
-            {
-                isClass = match.Groups["prefix"].Value.ToLower() == "class";
-                className = match.Groups["name"].Value;
-            }
-            // Else, try match suffix
-            else
-            {
-                match = SuffixPattern.Match(name);
-                isClass = match.Groups["suffix"].Value.ToLower().Trim() == "class";
-                className = name.Substring(0, name.Length - match.Groups["suffix"].Value.Length);
-            }
-
-            className = className.Trim();
-            return !string.IsNullOrWhiteSpace(className);
         }
 
     }
---
+++ Editor/Utilities/PrefixSuffixPattern.cs
@@ -0,0 +1,216 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using UnityEngine;
+
+namespace SideXP.AssetTemplates.EditorOnly
+{
+
+    /// <summary>
+    /// Utility for creating Regex patterns to detext prefix or suffix in a name.
+    /// </summary>
+    public class PrefixSuffixPattern
+    {
+
+        /// <summary>
+        /// Options for matching name parts.
+        /// </summary>
+        public struct PartOptions
+        {
+
+            /// <summary>
+            /// Default options used for matching name parts.
+            /// </summary>
+            public static readonly PartOptions Default = new PartOptions
+            {
+                FirstLetterCaseInsensitive = true,
+                MatchIfNextLetterUppercase = true,
+                RemovePart = true
+            };
+
+            /// <summary>
+            /// If enabled, this prefix or suffix matches whether it's used with a first lower or uppercase letter (other letters must
+            /// match your specification though).
+            /// </summary>
+            public bool FirstLetterCaseInsensitive;
+
+            /// <summary>
+            /// For prefixes only. If enabled, this prefix will match event if it's not followed by a space but by an uppercase letter.
+            /// </summary>
+            public bool MatchIfNextLetterUppercase;
+
+            /// <summary>
+            /// If enabled, this prefix or suffix will be removed from the input name if matched.
+            /// </summary>
+            public bool RemovePart;
+
+        }
+
+        private Dictionary<string, PartOptions> _prefixes = new Dictionary<string, PartOptions>();
+        private Dictionary<string, PartOptions> _suffixes = new Dictionary<string, PartOptions>();
+
+        /// <inheritdoc cref="PrefixSuffixPattern"/>
+        public PrefixSuffixPattern() { }
+
+        /// <inheritdoc cref="PrefixSuffixPattern"/>
+        /// <param name="prefixes"><inheritdoc cref="_prefixes" path="/summary"/></param>
+        /// <param name="suffixes"><inheritdoc cref="_suffixes" path="/summary"/></param>
+        public PrefixSuffixPattern(string[] prefixes, string[] suffixes)
+        {
+            foreach (string prefix in prefixes)
+                AddPrefix(prefix);
+            
+            foreach (string suffix in suffixes)
+                AddSuffix(suffix);
+        }
+
+        /// <inheritdoc cref="PrefixSuffixPattern"/>
+        /// <param name="parts">The name parts to match as both prefixes and suffixes.</param>
+        public PrefixSuffixPattern(params string[] parts)
+            : this (parts, parts) { }
+
+        /// <inheritdoc cref="AddPrefix(string, PartOptions)"/>
+        public void AddPrefix(string prefix)
+        {
+            AddPrefix(prefix, PartOptions.Default);
+        }
+
+        /// <summary>
+        /// Registers a prefix to match;
+        /// </summary>
+        /// <param name="prefix">The prefix to match.</param>
+        /// <param name="options">The options for making the prefix match.</param>
+        public void AddPrefix(string prefix, PartOptions options)
+        {
+            // Cancel if the prefix has already been registered
+            if (_prefixes.ContainsKey(prefix))
+                return;
+
+            _prefixes.Add(prefix, options);
+        }
+
+        /// <inheritdoc cref="AddSuffix(string, PartOptions)"/>
+        public void AddSuffix(string suffix)
+        {
+            AddSuffix(suffix, PartOptions.Default);
+        }
+
+        /// <summary>
+        /// Registers a suffix to match;
+        /// </summary>
+        /// <param name="suffix">The suffix to match.</param>
+        /// <param name="options">The options for making the suffix match.</param>
+        public void AddSuffix(string suffix, PartOptions options)
+        {
+            // Cancel if the suffix has already been registered
+            if (_suffixes.ContainsKey(suffix))
+                return;
+
+            _suffixes.Add(suffix, options);
+        }
+
+        /// <summary>
+        /// Checks if one of the defiend parts match.
+        /// </summary>
+        /// <param name="name">The name to match.</param>
+        /// <param name="outputName">Outputs the "final" name, after removing the matching part if needed.</param>
+        /// <param name="matchingPart">Outputs the part that validated the match.</param>
+        /// <returns>Returns true if one of the defined parts has matched.</returns>
+        public bool Match(string name, out string outputName, out string matchingPart)
+        {
+            name = name.Trim();
+
+            // Check for prefixes
+            foreach ((string part, PartOptions options) in _prefixes)
+            {
+                // Open group "prefix"
+                string pattern = "^(?<prefix>";
+                // Add a match for the exact part
+                pattern += $"(?:{part})";
+                // Add a match for the part with first letter case insensitive if applicable
+                if (options.FirstLetterCaseInsensitive)
+                {
+                    string partWithoutCasing = InverseFirstLetterCasing(part);
+                    pattern += $"|(?:{partWithoutCasing})";
+                }
+                // Close group "prefix"
+                pattern += ")";
+
+                // If allowed, add lookahead rule so the prefix matches only if followed by a space character or an uppercase letter
+                if (options.MatchIfNextLetterUppercase)
+                    pattern += @"(?=\s|[A-Z])";
+                // Else, add lookahead rule so the prefix matches only if followed by a space character
+                else
+                    pattern += @"(?=\s)";
+
+                // Use the built regex
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(name);
+                // Skip if the prefix doesn't match
+                if (!match.Success)
+                    continue;
+
+                matchingPart = match.Groups["prefix"].Value.Trim();
+                outputName = options.RemovePart
+                    ? name.Substring(match.Groups["prefix"].Value.Length)
+                    : name;
+                outputName.Trim();
+                return true;
+            }
+
+            // Check for suffixes
+            foreach ((string part, PartOptions options) in _suffixes)
+            {
+                // Open group "suffix", and non-capturing group to assert the whole pattern at the end
+                string pattern = "(?<suffix>(?:";
+                // Add a match for the exact part: if the part starts with a lowercase letter, it MUST be preceeded by a space character.
+                // If the part starts with an uppercase letter, it CAN be preceeded by a space character
+                pattern += $@"(?:\s{(char.IsLower(part[0]) ? @"+" : "*")}{part})";
+                // Add a match for the part with first letter case insensitive if applicable
+                if (options.FirstLetterCaseInsensitive)
+                {
+                    string partWithoutCasing = InverseFirstLetterCasing(part);
+                    pattern += $@"|(?:\s{(char.IsLower(partWithoutCasing[0]) ? @"+" : "*")}{partWithoutCasing})";
+                }
+                // Close the non-capturing group and group "suffix"
+                // Also asset the pattern at the end position
+                pattern += "))$";
+
+                // Use the built regex
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(name);
+                // Skip if the prefix doesn't match
+                if (!match.Success)
+                    continue;
+
+                matchingPart = match.Groups["suffix"].Value.Trim();
+                outputName = options.RemovePart
+                    ? name.Substring(0, match.Index)
+                    : name;
+                outputName.Trim();
+                return true;
+            }
+
+            outputName = name;
+            matchingPart = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Inverse the casing of the first letter of a given string: uppercase letter is changed to lower, and reverse.
+        /// </summary>
+        /// <param name="str">The string to process.</param>
+        /// <returns>Returns the processed string, with its first letter's casing inverted.</returns>
+        private string InverseFirstLetterCasing(string str)
+        {
+            char[] chars = str.ToCharArray();
+            if (char.IsUpper(chars[0]))
+                chars[0] = char.ToLower(chars[0]);
+            else
+                chars[0] = char.ToUpper(chars[0]);
+            return new string(chars);
+        }
+
+    }
+
+}
