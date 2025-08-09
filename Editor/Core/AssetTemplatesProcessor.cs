802967bae83ff16b1a7342649b5434bc123154d3
Hubert Grossin <hubert.grossin@gmail.com>
2025-08-09 15:16:06 +0200

    feat: automatic processor & utility

---
+++ Editor/Core/AssetTemplatesProcessor.cs
@@ -0,0 +1,164 @@
+using SideXP.Core;
+using SideXP.Core.EditorOnly;
+using SideXP.Core.Reflection;
+
+using System;
+using System.IO;
+using System.Reflection;
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+
+using Object = UnityEngine.Object;
+
+namespace SideXP.AssetTemplates.EditorOnly
+{
+
+    /// <summary>
+    /// Custom asset processor to trigger asset generation from templates.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The expected workflow is as follows:<br/>
+    /// - The user clicks on the menu at Assets > Create > Create Asset From Template<br/>
+    /// - The <see cref="CreateAssetFromTemplate"/> function is called, displaying a text field to name the asset<br/>
+    /// - The user types the name of the asset<br/>
+    /// - The "name edit" command is handled by an instance of <see cref="EndNameAssetTemplate"/> class, which will start iterating through
+    /// the exising asset template classes, and trigger the appropriate one if any
+    /// </para>
+    /// </remarks>
+    public class AssetTemplatesProcessor : AssetPostprocessor
+    {
+
+        // Menu
+        private const string MenuName = "Assets/Create/Create Asset From Template";
+        private const int MenuPriority = -220;
+        private const string DefaultFileName = "NewAsset";
+
+        // Defaults
+        private const string DefaultExtension = "cs";
+        private const string DefaultScriptTemplateFileName =
+#if UNITY_6000_0_OR_NEWER
+            "1-Scripting__MonoBehaviour Script-NewMonoBehaviourScript.cs.txt";
+#else
+            "81-C# Script-NewBehaviourScript.cs.txt";
+#endif
+
+        // Reflection
+
+        /// <summary>
+        /// The name of the function to invoke through reflection from <see cref="ProjectWindowUtil"/> to create a new script from a
+        /// template.
+        /// </summary>
+        private const string CreateScriptFromTemplateMethodName = "CreateScriptAssetFromTemplate";
+
+        // Paths
+        private const string TargetScriptTemplatesDirectory = "Data/Resources/ScriptTemplates";
+        private static readonly string ScriptTemplatesDirectory = null;
+
+        // Cache
+        private static Object s_selectedObjectBeforeAction = null;
+
+        /// <summary>
+        /// Represents the action to perform after naming a file to create using the template creation menu.
+        /// </summary>
+        private class EndNameAssetTemplate : EndNameEditAction
+        {
+
+            /// <inheritdoc cref="EndNameEditAction.Action(int, string, string)"/>
+            public override void Action(int instanceId, string pathName, string resourceFile)
+            {
+                string name = Path.GetFileNameWithoutExtension(pathName);
+                string extension = Path.GetExtension(pathName);
+
+                // Fix extension: use default extension if not defined, or just remove the "." character
+                extension = string.IsNullOrWhiteSpace(extension)
+                    ? DefaultExtension
+                    : extension.Substring(1);
+
+                // Fix path: ensure the extension is defined and the target path is unique
+                pathName = Path.GetDirectoryName(pathName);
+                pathName += $"/{name}.{extension}";
+                pathName = AssetDatabase.GenerateUniqueAssetPath(pathName);
+
+                // Process namespace
+                string namespaceStr = ScriptUtility.GetNamespaceFromPath(pathName);
+
+                // Process inheritance
+                string inheritFromPath = null;
+                if (ScriptUtility.TryGetDeclaredType(s_selectedObjectBeforeAction as TextAsset, out Type baseType))
+                    inheritFromPath = AssetDatabase.GetAssetPath(s_selectedObjectBeforeAction);
+
+                AssetInfo info = new AssetInfo(pathName, namespaceStr, inheritFromPath, baseType);
+                AssetOutputInfo output = new AssetOutputInfo { Path = info.Path };
+
+                // Try to generate the content from the appropriate asset template
+                foreach (Type assetTemplateType in AssetTemplatesUtility.GetAvailableAssetTemplateTypes())
+                {
+                    IAssetTemplate assetTemplate = AssetTemplatesUtility.GetAssetTemplateInstance(assetTemplateType);
+                    // Skip if the asset template is not valid or disabled
+                    if (assetTemplate == null || !AssetTemplatesUtility.IsEnabled(assetTemplate))
+                        continue;
+
+                    // Skip if the asset template can't generate the asset
+                    if (!assetTemplate.CanGenerateAsset(info))
+                        continue;
+
+                    if (!assetTemplate.GenerateAsset(info, ref output))
+                        Debug.LogError($"Failed to generate the asset from template \"{assetTemplateType.FullName}\"");
+
+                    break;
+                }
+
+                // Create the script depending on the generated content
+                if (!string.IsNullOrWhiteSpace(output.Content))
+                {
+                    if (output.Path.EndsWith(".cs"))
+                        ProjectWindowUtil.CreateScriptAssetWithContent(output.Path, output.Content);
+                    else
+                        ProjectWindowUtil.CreateAssetWithContent(output.Path, output.Content);
+                }
+                else
+                {
+                    MethodInfo createScriptFromTemplateMethod = typeof(ProjectWindowUtil).GetMethod(CreateScriptFromTemplateMethodName, ReflectionUtility.StaticFlags);
+                    // It's expected that the method takes two string parameters: the path to the file to create, and the path to the template file
+                    try
+                    {
+                        createScriptFromTemplateMethod.Invoke(null, new object[]
+                        {
+                            output.Path,
+                            $"{ScriptTemplatesDirectory}/{DefaultScriptTemplateFileName}"
+                        });
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogException(e);
+                        Debug.LogError("Failed to create a script file from the default template. See previous logs for more information.");
+                    }
+                }
+
+                s_selectedObjectBeforeAction = null;
+            }
+
+        }
+
+        /// <summary>
+        /// Static constructor.
+        /// </summary>
+        static AssetTemplatesProcessor()
+        {
+            ScriptTemplatesDirectory = PathUtility.ToPath($"{Path.GetDirectoryName(EditorApplication.applicationPath)}/{TargetScriptTemplatesDirectory}");
+        }
+
+        [MenuItem(MenuName, false, MenuPriority)]
+        private static void CreateAssetFromTemplate()
+        {
+            s_selectedObjectBeforeAction = Selection.activeObject;
+            var action = ScriptableObject.CreateInstance<EndNameAssetTemplate>();
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, action, DefaultFileName, null, null, true);
+        }
+
+    }
+
+}
